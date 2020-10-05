7aad6c7d26f5de84a167d31cd9b25974a4de63fd
Vinicius Souza <viisouza10@live.com>
2020-10-05 16:58:04 -0300

    styled components jest + storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,11 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
+
+import Main from '.'
+
+export default {
+    title: 'Main',
+    component: Main,
+    decorators: [withKnobs],
+}
+
+export const Basic = () => <Main title={text('Title', 'teste')} />
