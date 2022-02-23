4e4835b02f0005b351d0e45fccfde689c8bf1827
Vinicius Souza <viisouza10@live.com>
2022-02-23 10:11:39 -0300

    update package

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,11 @@
 import * as S from './styles'
 
 const Main = ({
-    title = 'React Avançado',
+    title = 'Boilerplate',
     description = 'Typescript, ReactJS, NextJS e Styled Components',
 }) => (
     <S.Wrapper>
-        <S.Logo
-            src="/img/logo.svg"
-            alt="Imagem de um átomo e React Avançado escrito ao lado"
-        />
+        <S.Logo src="/img/logo.svg" alt="Boilerplate" />
         <S.Title>{title}</S.Title>
         <S.Description>{description}</S.Description>
         <S.Illustration
